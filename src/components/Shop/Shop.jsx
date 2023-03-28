d1c8551e28fdbe5c83c66c893a4ce2c6b08cb5fd
Noushadozi <noushadozi333@gmail.com>
2023-03-28 11:09:29 +0600

    firstday

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -4,21 +4,30 @@ import './Shop.css';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect(()=>{
         fetch('products.json')
         .then(res=> res.json())
         .then(data => setProducts(data))
-    }, [])
+    }, []);
+
+    const handleAddToCart = (product) => {
+        // console.log('product added', product.name);
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
+
     return (
         <div className='shop-container'>
             <div className="products-container">
                {
-                products.map(product => <Product product={product} key={product.id}></Product>)
+                products.map(product => <Product product={product} key={product.id} handleAddToCart={handleAddToCart}></Product>)
                }
             </div>
             <div className="cart-container">
                 <h4>Order Summary</h4>
+                <p>Items in cart: {cart.length}</p>
             </div>
         </div>
     );
